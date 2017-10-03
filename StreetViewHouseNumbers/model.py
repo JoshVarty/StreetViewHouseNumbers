710ba1c89c03f72596120a8e2ba98e5d290039c1
Josh Varty <joshvarty@gmail.com>
2017-10-03 16:30:49 -0400

    Rework to pass in sequence lengths

--- StreetViewHouseNumbers/model.py
+++ StreetViewHouseNumbers/model.py
@@ -12,6 +12,7 @@ image_size = 64
 num_channels = 3
 pixel_depth = 255
 num_labels = 5
+num_digits = 10
 patch_size_3 = 3
 depth = 32
 
@@ -24,6 +25,8 @@ if not os.path.exists(digit_structure_path):
 digit_struct = DigitStruct(digit_structure_path)
 labels, paths = digit_struct.load_labels_and_paths()
 
+labels = labels[:1000]
+paths = paths[:1000]
 
 image_paths = [TRAIN_DIR + s for s in paths]
 images_normalized = image_helpers.prep_data(image_paths, image_size, num_channels, pixel_depth)
@@ -36,9 +39,23 @@ def randomize(dataset, labels):
   return shuffled_dataset, shuffled_labels
 
 
+def splitLengthsFromLabels(labels):
+    lengths = np.array(labels[:,0].tolist())
+    digit_labels = np.zeros((labels.shape[0], num_labels, num_digits))
+    for i in range(0, len(labels[:, 1:])):
+        current = labels[i, 1:]
+        reshaped = np.array(current.tolist())
+        digit_labels[i, :, :] = reshaped
+
+    return digit_labels, lengths
+
+
+
 train_dataset_rand, train_labels_rand = randomize(images_normalized, labels)
 train_images, valid_images, train_labels, valid_labels = train_test_split(train_dataset_rand, train_labels_rand, train_size=0.9, random_state=0)
 
+train_labels, train_lengths = splitLengthsFromLabels(train_labels) 
+valid_labels, valid_lengths = splitLengthsFromLabels(valid_labels) 
 
 def TrainConvNet():
 
@@ -58,8 +75,8 @@ def TrainConvNet():
     with graph.as_default():
         input = tf.placeholder(tf.float32, shape=(None, image_size, image_size, num_channels))
         
-        labels = tf.placeholder(tf.float32, shape=(None, 5, 11))
-        lengths = tf.placeholder(tf.float32, shape=(None, 1, 5))
+        labels = tf.placeholder(tf.float32, shape=(None, num_labels, num_digits))
+        lengths = tf.placeholder(tf.float32, shape=(None, 5))
         #keep_prob = tf.placeholder(tf.float32)
 
         #Conv->Relu->Conv->Relu->Pool
@@ -106,37 +123,38 @@ def TrainConvNet():
         b_l = weight_layer("b_l", [5])
         z_l = tf.matmul(h_fc, w_l) + b_l
 
-        w_s_1 = weight_layer("z_s_1", [hl, num_labels])
-        b_s_1 = bias_variable("b_s_1", [num_labels])
+        w_s_1 = weight_layer("z_s_1", [hl, num_digits])
+        b_s_1 = bias_variable("b_s_1", [num_digits])
         z_s_1 = tf.matmul(h_fc, w_s_1) + b_s_1
         
-        w_s_2 = weight_layer("z_s_2", [hl, num_labels])
-        b_s_2 = bias_variable("b_s_2", [num_labels])
+        w_s_2 = weight_layer("z_s_2", [hl, num_digits])
+        b_s_2 = bias_variable("b_s_2", [num_digits])
         z_s_2 = tf.matmul(h_fc, w_s_2) + b_s_2
         
-        w_s_3 = weight_layer("z_s_3", [hl, num_labels])
-        b_s_3 = bias_variable("b_s_3", [num_labels])
+        w_s_3 = weight_layer("z_s_3", [hl, num_digits])
+        b_s_3 = bias_variable("b_s_3", [num_digits])
         z_s_3 = tf.matmul(h_fc, w_s_3) + b_s_3
 
-        w_s_4 = weight_layer("z_s_4", [hl, num_labels])
-        b_s_4 = bias_variable("b_s_4", [num_labels])
+        w_s_4 = weight_layer("z_s_4", [hl, num_digits])
+        b_s_4 = bias_variable("b_s_4", [num_digits])
         z_s_4 = tf.matmul(h_fc, w_s_4) + b_s_4
 
-        w_s_5 = weight_layer("z_s_5", [hl, num_labels])
-        b_s_5 = bias_variable("b_s_5", [num_labels])
+        w_s_5 = weight_layer("z_s_5", [hl, num_digits])
+        b_s_5 = bias_variable("b_s_5", [num_digits])
         z_s_5 = tf.matmul(h_fc, w_s_5) + b_s_5
 
-        labels1 = tf.squeeze(tf.slice(labels, [0, 0, 0], [-1, 1, 11]), axis=1)
-        labels2 = tf.squeeze(tf.slice(labels, [0, 1, 0], [-1, 1, 11]), axis=1)
-        labels3 = tf.squeeze(tf.slice(labels, [0, 2, 0], [-1, 1, 11]), axis=1)
-        labels4 = tf.squeeze(tf.slice(labels, [0, 3, 0], [-1, 1, 11]), axis=1)
-        labels5 = tf.squeeze(tf.slice(labels, [0, 4, 0], [-1, 1, 11]), axis=1)
+        labels1 = tf.squeeze(tf.slice(labels, [0, 0, 0], [-1, 1, num_digits]), axis=1)
+        labels2 = tf.squeeze(tf.slice(labels, [0, 1, 0], [-1, 1, num_digits]), axis=1)
+        labels3 = tf.squeeze(tf.slice(labels, [0, 2, 0], [-1, 1, num_digits]), axis=1)
+        labels4 = tf.squeeze(tf.slice(labels, [0, 3, 0], [-1, 1, num_digits]), axis=1)
+        labels5 = tf.squeeze(tf.slice(labels, [0, 4, 0], [-1, 1, num_digits]), axis=1)
 
-        five_gate = tf.minimum(lengths[:,0,4], [1.0])
-        four_gate = tf.maximum(lengths[:,0,3], five_gate)
-        three_gate = tf.maximum(lengths[:,0,2], four_gate)
-        two_gate = tf.maximum(lengths[:,0,1], three_gate)
-        one_gate = tf.maximum(lengths[:,0,0], two_gate)
+        #We don't want to backpropagate cost on outputs that are 
+        five_gate = tf.minimum(lengths[:,4], [1.0])
+        four_gate = tf.maximum(lengths[:,3], five_gate)
+        three_gate = tf.maximum(lengths[:,2], four_gate)
+        two_gate = tf.maximum(lengths[:,1], three_gate)
+        one_gate = tf.maximum(lengths[:,0], two_gate)
 
         cost_length = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=lengths, logits=z_l))
         cost1 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels1, logits=z_s_1)) * one_gate
@@ -147,13 +165,13 @@ def TrainConvNet():
 
         total_cost = cost_length + cost1 + cost2 + cost3 + cost4 + cost5
 
+        length_prediction = tf.nn.softmax(z_l)
         train_prediction = tf.stack([
-            tf.nn.softmax(z_l),
             tf.nn.softmax(z_s_1),
             tf.nn.softmax(z_s_2),
             tf.nn.softmax(z_s_3),
             tf.nn.softmax(z_s_4),
-            tf.nn.softmax(z_s_5),
+            tf.nn.softmax(z_s_5)
             ], axis=1)
 
         optimizer = tf.train.AdamOptimizer(0.00001).minimize(total_cost)
@@ -168,11 +186,12 @@ def TrainConvNet():
                 offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
                 batch_data = train_images[offset:(offset + batch_size), :, :, :]
                 batch_labels = train_labels[offset:(offset + batch_size), :]
+                batch_lengths = train_lengths[offset:(offset + batch_size), :]
 
-                feed_dict = {input : batch_data, labels : batch_labels}
+                feed_dict = {input : batch_data, labels : batch_labels, lengths: batch_lengths}
 
                 if step % 500 == 0:
-                    _, l, predictions = session.run([optimizer, total_cost, train_prediction], feed_dict=feed_dict)
+                    _, l, predictions, length_preds = session.run([optimizer, total_cost, train_prediction, length_prediction], feed_dict=feed_dict)
                     print('Minibatch loss at step %d: %f' % (step, l))
                     print('Minibatch accuracy: %.1f%%' % accuracy(predictions, batch_labels))
                     #Validation
