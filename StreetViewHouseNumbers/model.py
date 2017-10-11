b5bf4f7344e20d110443cbc20b425ccd8551da23
Josh Varty <joshvarty@gmail.com>
2017-10-11 15:32:20 -0400

    Add scopes

--- StreetViewHouseNumbers/model.py
+++ StreetViewHouseNumbers/model.py
@@ -44,103 +44,68 @@ def TrainConvNet():
         #keep_prob = tf.placeholder(tf.float32)
 
         #Conv->Relu->Conv->Relu->Pool
-        w_conv1 = weight_layer("w_conv1", [patch_size_3, patch_size_3, num_channels, depth])
-        b_conv1 = bias_variable("b_conv1", [depth])
-        h_conv1 = conv2d_relu(input, w_conv1, b_conv1)
-        w_conv2 = weight_layer("w_conv2", [patch_size_3, patch_size_3, depth, depth])
-        b_conv2 = bias_variable("b_conv2", [depth])
-        h_conv2 = conv2d_relu(h_conv1, w_conv2, b_conv2)
-        h_pool2 = max_pool_2x2(h_conv2)
+        with tf.name_scope("Layer1"):
+            w_conv1 = weight_layer("w_conv1", [patch_size_3, patch_size_3, num_channels, depth])
+            b_conv1 = bias_variable("b_conv1", [depth])
+            h_conv1 = conv2d_relu(input, w_conv1, b_conv1)
+            w_conv2 = weight_layer("w_conv2", [patch_size_3, patch_size_3, depth, depth])
+            b_conv2 = bias_variable("b_conv2", [depth])
+            h_conv2 = conv2d_relu(h_conv1, w_conv2, b_conv2)
+            h_pool2 = max_pool_2x2(h_conv2)
 
         #Conv->Relu->Conv->Relu->Pool
-        w_conv3 = weight_layer("w_conv3", [patch_size_3, patch_size_3, depth, depth * 2])
-        b_conv3 = bias_variable("b_conv3", [depth * 2])
-        h_conv3 = conv2d_relu(h_pool2, w_conv3, b_conv3)
-        w_conv4 = weight_layer("w_conv4", [patch_size_3, patch_size_3, depth * 2, depth * 4])
-        b_conv4 = bias_variable("b_conv4", [depth * 4])
-        h_conv4 = conv2d_relu(h_conv3, w_conv4, b_conv4)
-        h_pool4 = max_pool_2x2(h_conv4)
+        with tf.name_scope("Layer2"):
+            w_conv3 = weight_layer("w_conv3", [patch_size_3, patch_size_3, depth, depth * 2])
+            b_conv3 = bias_variable("b_conv3", [depth * 2])
+            h_conv3 = conv2d_relu(h_pool2, w_conv3, b_conv3)
+            w_conv4 = weight_layer("w_conv4", [patch_size_3, patch_size_3, depth * 2, depth * 4])
+            b_conv4 = bias_variable("b_conv4", [depth * 4])
+            h_conv4 = conv2d_relu(h_conv3, w_conv4, b_conv4)
+            h_pool4 = max_pool_2x2(h_conv4)
 
         #Conv->Relu->Conv->Relu->Conv->Relu->Pool
-        w_conv5 = weight_layer("w_conv5", [patch_size_3, patch_size_3, depth * 4, depth * 4])
-        b_conv5 = bias_variable("b_conv5", [depth * 4])
-        h_conv5 = conv2d_relu(h_pool4, w_conv5, b_conv5)
-        w_conv6 = weight_layer("w_conv6", [patch_size_3, patch_size_3, depth * 4, depth * 4])
-        b_conv6 = bias_variable("b_conv6", [depth * 4])
-        h_conv6 = conv2d_relu(h_conv5, w_conv6, b_conv6)
-        w_conv7 = weight_layer("w_conv7", [patch_size_3, patch_size_3, depth * 4, depth * 8])
-        b_conv7 = bias_variable("b_conv7", [depth * 8])
-        h_conv7 = conv2d_relu(h_conv6, w_conv7, b_conv7)
-        h_pool7 = max_pool_2x2(h_conv7)
+        with tf.name_scope("Layer3"):
+            w_conv5 = weight_layer("w_conv5", [patch_size_3, patch_size_3, depth * 4, depth * 4])
+            b_conv5 = bias_variable("b_conv5", [depth * 4])
+            h_conv5 = conv2d_relu(h_pool4, w_conv5, b_conv5)
+            w_conv6 = weight_layer("w_conv6", [patch_size_3, patch_size_3, depth * 4, depth * 4])
+            b_conv6 = bias_variable("b_conv6", [depth * 4])
+            h_conv6 = conv2d_relu(h_conv5, w_conv6, b_conv6)
+            w_conv7 = weight_layer("w_conv7", [patch_size_3, patch_size_3, depth * 4, depth * 8])
+            b_conv7 = bias_variable("b_conv7", [depth * 8])
+            h_conv7 = conv2d_relu(h_conv6, w_conv7, b_conv7)
+            h_pool7 = max_pool_2x2(h_conv7)
         
         #Conv->Relu->Conv->Relu->Conv->Relu->Pool
-        w_conv8 = weight_layer("w_conv8", [patch_size_3, patch_size_3, depth * 8, depth * 8])
-        b_conv8 = bias_variable("b_conv8", [depth * 8])
-        h_conv8 = conv2d_relu(h_pool7, w_conv8, b_conv8)
-        w_conv9 = weight_layer("w_conv9", [patch_size_3, patch_size_3, depth * 8, depth * 8])
-        b_conv9 = bias_variable("b_conv9", [depth * 8])
-        h_conv9 = conv2d_relu(h_conv8, w_conv9, b_conv9)
-        w_conv10 = weight_layer("w_conv10", [patch_size_3, patch_size_3, depth * 8, depth * 16])
-        b_conv10 = bias_variable("b_conv10", [depth * 16])
-        h_conv10 = conv2d_relu(h_conv9, w_conv10, b_conv10)
-        h_pool10 = max_pool_2x2(h_conv10)
+        with tf.name_scope("Layer4"):
+            w_conv8 = weight_layer("w_conv8", [patch_size_3, patch_size_3, depth * 8, depth * 8])
+            b_conv8 = bias_variable("b_conv8", [depth * 8])
+            h_conv8 = conv2d_relu(h_pool7, w_conv8, b_conv8)
+            w_conv9 = weight_layer("w_conv9", [patch_size_3, patch_size_3, depth * 8, depth * 8])
+            b_conv9 = bias_variable("b_conv9", [depth * 8])
+            h_conv9 = conv2d_relu(h_conv8, w_conv9, b_conv9)
+            w_conv10 = weight_layer("w_conv10", [patch_size_3, patch_size_3, depth * 8, depth * 16])
+            b_conv10 = bias_variable("b_conv10", [depth * 16])
+            h_conv10 = conv2d_relu(h_conv9, w_conv10, b_conv10)
+            h_pool10 = max_pool_2x2(h_conv10)
 
         #Dropout -> Fully Connected -> Dropout -> Fully Connected
-        drop_1 = tf.nn.dropout(h_pool10, 1.0)
-        shape = drop_1.get_shape().as_list()
-        reshape = tf.reshape(drop_1, [-1, shape[1] * shape[2] * shape[3]])
+        with tf.name_scope("FullyConnected"):
+            drop_1 = tf.nn.dropout(h_pool10, 1.0)
+            shape = drop_1.get_shape().as_list()
+            reshape = tf.reshape(drop_1, [-1, shape[1] * shape[2] * shape[3]])
 
-        fc = 1024
-        hl = 4096
-        w_fc = weight_layer("w_fc", [fc, hl])
-        b_fc = bias_variable("b_fc", [hl])
-        h_fc = tf.matmul(reshape, w_fc) + b_fc
+            fc = 1024
+            hl = 4096
+            w_fc = weight_layer("w_fc", [fc, hl])
+            b_fc = bias_variable("b_fc", [hl])
+            h_fc = tf.matmul(reshape, w_fc) + b_fc
 
-        #w_l = weight_layer("w_l", [hl, 5])
-        #b_l = weight_layer("b_l", [5])
-        #z_l = tf.matmul(h_fc, w_l) + b_l
-
-        w_s_1 = weight_layer("z_s_1", [hl, num_digits])
-        b_s_1 = bias_variable("b_s_1", [num_digits])
-        z_s_1 = tf.matmul(h_fc, w_s_1) + b_s_1
-        
-        #w_s_2 = weight_layer("z_s_2", [hl, num_digits])
-        #b_s_2 = bias_variable("b_s_2", [num_digits])
-        #z_s_2 = tf.matmul(h_fc, w_s_2) + b_s_2
-        
-        #w_s_3 = weight_layer("z_s_3", [hl, num_digits])
-        #b_s_3 = bias_variable("b_s_3", [num_digits])
-        #z_s_3 = tf.matmul(h_fc, w_s_3) + b_s_3
-
-        #w_s_4 = weight_layer("z_s_4", [hl, num_digits])
-        #b_s_4 = bias_variable("b_s_4", [num_digits])
-        #z_s_4 = tf.matmul(h_fc, w_s_4) + b_s_4
-
-        #w_s_5 = weight_layer("z_s_5", [hl, num_digits])
-        #b_s_5 = bias_variable("b_s_5", [num_digits])
-        #z_s_5 = tf.matmul(h_fc, w_s_5) + b_s_5
-
-        #labels1 = tf.slice(labels, [0, 0], [-1, 1])
-        #labels2 = tf.slice(labels, [0, 1], [-1, 1])
-        #labels3 = tf.slice(labels, [0, 2], [-1, 1])
-        #labels4 = tf.slice(labels, [0, 3], [-1, 1])
-        #labels5 = tf.slice(labels, [0, 4], [-1, 1])
+            w_s_1 = weight_layer("z_s_1", [hl, num_digits])
+            b_s_1 = bias_variable("b_s_1", [num_digits])
+            z_s_1 = tf.matmul(h_fc, w_s_1) + b_s_1
 
         cost = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=z_s_1))
-        #cost2 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels2, logits=z_s_2))
-        #cost3 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels3, logits=z_s_3))
-        #cost4 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels4, logits=z_s_4))
-        #cost5 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels5, logits=z_s_5))
-
-        #total_cost = cost1 + cost2 + cost3 + cost4 + cost5
-
-        #train_prediction = tf.stack([
-        #    tf.nn.softmax(z_s_1),
-        #    tf.nn.softmax(z_s_2),
-        #    tf.nn.softmax(z_s_3),
-        #    tf.nn.softmax(z_s_4),
-        #    tf.nn.softmax(z_s_5)
-        #    ], axis=1)
 
         train_prediction = tf.nn.softmax(z_s_1)
 
