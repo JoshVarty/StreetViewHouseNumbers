eddf5befd260ca91a7572d0e15ae755248dbbab6
Josh Varty <joshvarty@gmail.com>
2017-11-10 15:56:06 -0500

    Include driver

---
+++ StreetViewHouseNumbers/driver.py
@@ -0,0 +1,9 @@
+import resnet_module
+
+
+resnet_module.TrainConvNet(0.0001)
+resnet_module.TrainConvNet(0.001)
+resnet_module.TrainConvNet(0.01)
+resnet_module.TrainConvNet(0.1)
+resnet_module.TrainConvNet(1)
+resnet_module.TrainConvNet(2)
